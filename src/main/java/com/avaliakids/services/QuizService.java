e1264ec653c1ae9d6509447d62b729382fb33cec
rafael-andrade-qa <142061101+rafael-iftm@users.noreply.github.com>
2025-02-16 11:43:38 -0300

    Update QuizService.java

--- src/main/java/com/avaliakids/services/QuizService.java
+++ src/main/java/com/avaliakids/services/QuizService.java
@@ -1,18 +1,23 @@
 package com.avaliakids.services;
 
+import com.avaliakids.models.Question;
 import com.avaliakids.models.QuizAnswer;
+import com.avaliakids.repositories.QuestionRepository;
 import com.avaliakids.repositories.QuizAnswerRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
     
     private final QuizAnswerRepository quizAnswerRepository;
+    private final QuestionRepository questionRepository;
 
-    public QuizService(QuizAnswerRepository quizAnswerRepository) {
+    public QuizService(QuizAnswerRepository quizAnswerRepository, QuestionRepository questionRepository) {
         this.quizAnswerRepository = quizAnswerRepository;
+        this.questionRepository = questionRepository;
     }
 
     public boolean hasStudentAlreadyAnswered(String studentId, String questionId) {
@@ -23,8 +28,21 @@ public class QuizService {
         if (hasStudentAlreadyAnswered(answer.getStudentId(), answer.getQuestionId())) {
             return false;
         }
-        quizAnswerRepository.save(answer);
-        return true;
+
+        Optional<Question> questionOpt = questionRepository.findById(answer.getQuestionId());
+
+        if (questionOpt.isPresent()) {
+            Question question = questionOpt.get();
+
+            boolean isCorrect = question.getCorrectOption().trim().equalsIgnoreCase(answer.getSelectedOption().trim());
+
+            answer.setCorrect(isCorrect);
+
+            quizAnswerRepository.save(answer);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public List<QuizAnswer> getAnswersByStudent(String studentId) {
